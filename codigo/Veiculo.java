4de42b9833563ee56feedd8a01b15714ab2b89cf
j0a0N3ri <joao.moreira.1454677@sga.pucminas.br>
2023-10-16 17:56:21 -0300

    atulizando a classe Veiculo

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,45 +1,58 @@
 
-
 public class Veiculo {
 
 	private String placa;
 	private UsoDeVaga[] usos;
 
-	public string getPlaca(){
-		return placa;
-	}
-	public UsodeVaga[] getUsos(){
-		return usos;
-	}
-	public void setPlaca(String placa){
+	
+	public Veiculo(String placa, UsoDeVaga[] usos) {
 		this.placa = placa;
-	}
-	public void setUsos(UsoDeVaga[] usos){
 		this.usos = usos;
 	}
 
+	
+
 	public Veiculo(String placa) {
-		
+
 	}
 
 	public void estacionar(Vaga vaga) {
-		
+
 	}
 
 	public double sair() {
-		
+		return 0.0;
 	}
+	
 
 	public double totalArrecadado() {
-		
+		return 0.0;
 	}
+	
 
 	public double arrecadadoNoMes(int mes) {
-		
+		return 0.0;
 	}
 
 	public int totalDeUsos() {
-		
+		return 1;
+
 	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public UsoDeVaga[] getUsos() {
+		return usos;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public void setUsos(UsoDeVaga[] usos) {
+		this.usos = usos;
+	}
+
 
 }
