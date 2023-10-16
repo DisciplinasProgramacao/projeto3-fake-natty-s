0fec0b465c63a97c271a3c3c8ae00138382467b6
felipe picinin <138057342+picininz@users.noreply.github.com>
2023-10-16 20:37:39 -0300

    5 metodos Estacionamento.java #9

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,4 +1,5 @@
-
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Estacionamento {
 
@@ -7,9 +8,14 @@ public class Estacionamento {
 	private Vaga[] vagas;
 	private int quantFileiras;
 	private int vagasPorFileira;
+	private int valorTotal;
+	private int valorMes;
+	private int valorUso;
 
 	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
+		this.valorTotal = 0;
+		this.valorMes = 0;
+		this.valorUso = 0;
 	}
 
 	public void addVeiculo(Veiculo veiculo, String idCli) {
@@ -29,49 +35,53 @@ public class Estacionamento {
 	}
 
 	public double sair(String placa) {
-		
+		for (Vaga vaga : vagas){
+			if(vaga.disponivel()) {
+				vaga.sair()
+				break;
+			}
+		}
 	}
 
 	public double totalArrecadado() {
-		
+		for (Cliente cliente : clientes) {
+			valorTotal += cliente.getArrecadoTotal();
+		}
+		return valorTotal;
 	}
 
 	public double arrecadacaoNoMes(int mes) {
-		
+		for (Cliente cliente : clientes) {
+			 int mesCliente = cliente.GetData().getMonth();
+			 if (mesCliente == mes) {
+				 valorMes += cliente.getArrecadoNoMes();
+			 }
+			 return valorMes;
+		}
 	}
 
 	public double valorMedioPorUso() {
-		
+		for (Cliente cliente : clientes) {
+			valorUso = cliente.getArrecadoPorVeiculo() / cliente.getTotalDeUsos();
+		}
+		return valorUso;
 	}
 
 	public String top5Clientes(int mes) {
-		
-	}
+		Cliente[] clientesOrdenados = Arrays.copyOf(clientes, clientes.length);
+    Arrays.sort(clientesOrdenados, new Comparator<Cliente>() {
+        @Override
+        public int compare(Cliente c1, Cliente c2) {
+            return c2.totalDeUsos() - c1.totalDeUsos();
+        }
+    });
+	StringBuilder top5 = new StringBuilder("Top 5 Clientes:\n");
+    for (int i = 0; i < 5 && i < clientesOrdenados.length; i++) {
+        Cliente cliente = clientesOrdenados[i];
+        top5.append("Nome: ").append(cliente.getNome()).append(", Total de Usos: ").append(cliente.totalDeUsos()).append("\n");
+    }
 
-	public String getNome(){
-		return nome;
+    return top5.toString();
 	}
-	
-	public void setNome(String nome){
-		this.nome = nome;
-	}
-
-	public int getQuantFileiras(){
-		return quantFileiras;
-	}
-
-	public void setQuantFIleiras(int quantFileiras){
-		this.quantFileiras = quantFileiras;
-	}
-
-	public int getVagasPorFileira(){
-		return vagasPorFileira;
-	}
-
-	public void setVagasPorFIleira(int vagasPorFileira){
-		this.vagasPorFileira = vagasPorFileira;
-	}
-
-	
 
 }
