84d1669fa031ffe0c5a2dafd1b1a69bb2caaec7e
renatoctti <130700804+renatoctti@users.noreply.github.com>
2023-10-17 15:04:53 -0300

    atualizando a classe cliente #2

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,59 +1,84 @@
-
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cliente {
 
-	private String nome;
-	private String id;
-	private Veiculo[] veiculos;
+    private String nome;
+    private String id;
+    private List<Veiculo> veiculos;
 
-	public Cliente(String nome, String id) {
-		
-	}
+    public Cliente(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+    }
 
-	public void addVeiculo(Veiculo veiculo) {
-		
-	}
+    public void addVeiculo(Veiculo veiculo) {
+        veiculos.add(veiculo);
+    }
 
-	public Veiculo possuiVeiculo(String placa) {
-		
-	}
-
-	public int totalDeUsos() {
-		
-	}
-
-	public double arrecadadoPorVeiculo(String placa) {
-		
-	}
-
-	public double arrecadadoTotal() {
-		
-	}
-
-	public double arrecadadoNoMes(int mes) {
-		
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public Veiculo[] getVeiculos() {
-		return veiculos;
-	}
-	public void setVeiculos(Veiculo[] veiculos) {
-		this.veiculos = veiculos;
-	}
+    public Veiculo possuiVeiculo(String placa) {
+    for (Veiculo veiculo : veiculos) {
+        if (veiculo.getPlaca().equals(placa)) {
+            return veiculo; 
+        }
+    }
+    return null; 
+    }
 	
+	public int totalDeUsos() {
+        int total = 0;
+        for (Veiculo veiculo : veiculos) {
+            total += veiculos.getUsos();
+        }
+        return total;
+    }
 
+    public double arrecadadoPorVeiculo(String placa) {
+        for (Veiculo veiculo : veiculos) {
+            if (veiculo.getPlaca().equals(placa)) {
+                return veiculo.totalArrecadado();
+            }
+        }
+        return 0.0;
+    }
+
+    public double arrecadadoTotal() {
+        double totalArrecadado = 0.0;
+        for (Veiculo veiculo : veiculos) {
+            totalArrecadado += veiculo.totalArrecadado();
+        }
+        return totalArrecadado;
+    }
+
+    public double arrecadadoNoMes(int mes) {
+        double arrecadadoMes = 0.0;
+        for (Veiculo veiculo : veiculos) {
+            arrecadadoMes += veiculo.arrecadadoNoMes(mes);
+        }
+        return arrecadadoMes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ArrayList<Veiculo> getVeiculos() {
+        return (ArrayList<Veiculo>) veiculos;
+    }
+
+    public void setVeiculos(ArrayList<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
 }
