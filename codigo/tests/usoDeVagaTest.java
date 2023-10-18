97e26b61053a21a788d99445a21fa1b18678cd4e
felipe picinin <138057342+picininz@users.noreply.github.com>
2023-10-18 11:15:44 -0300

    Update usoDeVagaTest.java #14

--- codigo/tests/usoDeVagaTest.java
+++ codigo/tests/usoDeVagaTest.java
@@ -1,5 +1,33 @@
 package tests;
+ 
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class usoDeVagaTest {
-    
+
+    private UsoDeVaga uso;
+    private Vaga vaga;
+
+    @Before
+    public void setUp() {
+        vaga = new Vaga("Vaga de Teste");
+        uso = new UsoDeVaga(vaga);
+    }
+
+    @Test
+    public void testSair() {
+        double valorPago = uso.sair();
+        assertEquals(true, valorPago > 0);
+    }
+   
+    @Test
+    public void testValorPago() {
+        assertEquals(0.0, uso.valorPago(), 0.01);
+        double valorPago = uso.sair();
+        assertEquals(true, uso.valorPago() > 0);
+    }
+
 }
