47f69bfca2b37040537809bc20cb4e66bbb6d5b2
oarthurfc <arthuryouface@gmail.com>
2023-10-23 19:14:05 -0300

    Corrigindo Servicos adicionais em UsoDeVagas #33

---
+++ codigo/src/ServicosAdicionais.java
@@ -0,0 +1,29 @@
+package src;
+
+public enum ServicosAdicionais {
+    MANOBRISTA("Manobrista", 5.0, 0),
+    LAVAGEM("Lavagem", 20.0, 60),
+    POLIMENTO("Polimento (inclui lavagem)", 45.0, 120);
+
+    private final String descricao;
+    private final double valor;
+    private final int tempoMinimo;
+
+    ServicosAdicionais(String descricao, double valor, int tempoMinimo) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tempoMinimo = tempoMinimo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int getTempoMinimo() {
+        return tempoMinimo;
+    }
+}
