6e2132bd25c525dc1262545d3040ea17f043b8f4
BLpedro <pedrobarrolemos813@gmail.com>
2023-10-31 21:17:13 -0300

    atualizçao vaga.java

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -1,47 +1,71 @@
+package src;
 import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * A classe Vaga representa uma vaga de estacionamento em um estacionamento.
+ */
 public class Vaga {
 
-	private String fila;
-	private int numero;
-	private String id;
-	private boolean disponivel;
-	private String conversor;
-	private List<Vaga> vagas;
+    private String fila;
+    private int numero;
+    private boolean disponivel;
+    /**
+     * Construtor da classe Vaga.
+     *
+     * @param fila    Uma string representando a fila da vaga.
+     * @param numero  Um número inteiro representando o número da vaga.
+     */
+    public Vaga(String fila, int numero) {
+        this.fila = fila;
+        this.numero = numero;
+        new ArrayList<Vaga>();
+        this.disponivel = true;
+        String.valueOf(numero);
+    }
 
-	public Vaga(String fila, int numero) {
-		this.fila = fila;
-		this.numero = numero;
-		this.vagas = new ArrayList<Vaga>();
-		this.disponivel = true;
-		this.conversor = String.valueOf(numero); // conversor de int pra string
-		this.id = fila + numero; //
+    /**
+     * Valida se o carro pode ser estacionado na vaga.
+     *
+     * @return true se o carro pode ser estacionado, ou false se a vaga já está ocupada.
+     */
+    public boolean estacionar() {
+        if (disponivel) {
+            disponivel = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	}
+    /**
+     * Verifica se a vaga está disponível e permite que o usuário saia dela.
+     *
+     * @return true se o usuário puder sair da vaga, ou false se a vaga estiver vazia.
+     */
+    public boolean sair() {
+        if (!disponivel) {
+            disponivel = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public boolean estacionar() {
-		if (!disponivel) {
-			disponivel = false;
-			return true;
-		} else {
-			return false;
-		}
-	}
+    /**
+     * Obtém a posição da vaga, representada como uma combinação da fila e número.
+     *
+     * @return Uma string representando a posição da vaga.
+     */
+    public String getPosicao() {
+        return this.fila + numero;
+    }
 
-	public boolean sair() {
-		if (!disponivel) {
-			disponivel = true;
-			return true;
-		} else {
-			return false;
-		}
-
-	}
-
-	public boolean disponivel() {
-		return disponivel;
-
-	}
-
+    /**
+     * Verifica se a vaga está disponível.
+     *
+     * @return true se a vaga estiver disponível, ou false se estiver ocupada.
+     */
+    public boolean disponivel() {
+        return disponivel;
+    }
 }
