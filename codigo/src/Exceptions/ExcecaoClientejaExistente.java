5ba9ed6d6393a71afb0d85082ed2e71001e79ed8
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-11-21 20:05:58 -0300

    Update ExcecaoClientejaExistente.java
    #37

--- codigo/src/Exceptions/ExcecaoClientejaExistente.java
+++ codigo/src/Exceptions/ExcecaoClientejaExistente.java
@@ -1,4 +1,4 @@
-package src.exceptions;
+package src.Exceptions;
 
 import src.entities.Cliente;
 
@@ -7,4 +7,9 @@ public class ExcecaoClientejaExistente extends Exception{
     public ExcecaoClientejaExistente(Cliente cliente){
         super("o Cliente " + cliente.getNome() + "ja existe no sistema, portanto o id " + cliente.getId() + "ja está cadastrado");
     }
+
+    public ExcecaoClientejaExistente(String msg){
+        super(msg);
+    }
+    
 }
