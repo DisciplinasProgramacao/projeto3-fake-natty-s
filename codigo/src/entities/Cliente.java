94f860a6fd4184657e721a3281bd9c176be015f4
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-11-22 10:52:37 -0300

    Update Cliente.java

--- codigo/src/entities/Cliente.java
+++ codigo/src/entities/Cliente.java
@@ -19,6 +19,8 @@ public class Cliente implements Serializable, Entidade {
     private String id;
     private List<Veiculo> veiculos;
     private List<Estacionamento> estacionamentos = new ArrayList<>();
+    private ModalidadeCliente modalidade;
+    private Turno turnoEscolhido;
 
 
     /**
