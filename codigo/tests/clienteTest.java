3adc02b1342a06d1310efa91a236f6dfe9d580c3
oarthurfc <arthuryouface@gmail.com>
2023-11-29 10:26:12 -0300

    Teste clase cliente

--- codigo/tests/clienteTest.java
+++ codigo/tests/clienteTest.java
@@ -1,7 +1,9 @@
 package tests;
 import src.*;
 import src.entities.Cliente;
+import src.entities.Estacionamento;
 import src.entities.Veiculo;
+import src.exceptions.ExcecaoClientejaExistente;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,14 +15,17 @@ public class clienteTest {
     private Cliente cliente;
     private Veiculo veiculo1;
     private Veiculo veiculo2;
+    private Estacionamento estacionamento;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws ExcecaoClientejaExistente {
         cliente = new Cliente("Cliente Teste", "123");
         veiculo1 = new Veiculo("ABC123");
         veiculo2 = new Veiculo("DEF456");
+        estacionamento = new Estacionamento("Estacionamento Teste", 10, 10);
         cliente.addVeiculo(veiculo1);
         cliente.addVeiculo(veiculo2);
+        estacionamento.addCliente(cliente);
     }
 
     @Test
@@ -28,6 +33,12 @@ public class clienteTest {
         Veiculo novoVeiculo = new Veiculo("GHI789");
         cliente.addVeiculo(novoVeiculo);
         assertEquals(3, cliente.getVeiculos().size());
+    }
+
+    @Test
+    public void testPossuiVeiculo() {
+        Veiculo veiculoCliente = cliente.possuiVeiculo("ABC123");
+        assertEquals(veiculoCliente, veiculo1);
     }
 
     @Test
