8b997c231d2ab762dece684ae31a936683e7a898
felipe picinin <138057342+picininz@users.noreply.github.com>
2023-12-11 11:49:26 -0300

    observer no Relatorio.java #85

--- codigo/src/entities/Relatorio.java
+++ codigo/src/entities/Relatorio.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import src.exceptions.ExcecaoMesInvalido;
 
-public class Relatorio extends Observer {
+public class Relatorio implements Observer {
     List<Cliente> observers;
 
+    @Override
+    public void notificar(){
+        top5clientesMes(mes);
+    }
+    
+    
     public String top5clientesMes(int mes) throws ExcecaoMesInvalido {
         
         if (mes < 1 || mes > 12) {
