822afa5d22ba0b7840f3b30da077ae2d0b4a8e6a
felipe picinin <138057342+picininz@users.noreply.github.com>
2023-12-11 11:47:09 -0300

    criando relatorio implementando observer Relatorio.java #85

---
+++ codigo/Relatorio.java
@@ -0,0 +1,25 @@
+package src.entities;
+
+import java.util.List;
+
+import src.exceptions.ExcecaoMesInvalido;
+
+public class Relatorio implements Observer {
+    List<Cliente> observers;
+
+    @Override
+    public void notificar(){
+        top5clientesMes(mes);
+    }
+    
+    
+    public String top5clientesMes(int mes) throws ExcecaoMesInvalido {
+        
+        if (mes < 1 || mes > 12) {
+            throw new ExcecaoMesInvalido();
+        }
+
+        return "clientes";
+    }
+    
+}
