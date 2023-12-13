5f28410ee7ed03f91e43fa46dd14a7e21dd85cd1
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-12-13 02:46:40 -0300

    Update UsoCarroFactoryImp.java

--- codigo/src/entities/UsoCarroFactoryImp.java
+++ codigo/src/entities/UsoCarroFactoryImp.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 import src.enums.ServicosAdicionais;
 import src.interfaces.UsoDeVaga;
+import src.interfaces.UsoDeVagaFactory;
 
-public class UsoCarroFactoryImp {
-    UsoDeVaga criar(Vaga vaga, LocalDateTime entrada, LocalDateTime saida, double valorPago,
+public class UsoCarroFactoryImp implements UsoDeVagaFactory{
+    public UsoDeVaga criarUsoDeVaga(Vaga vaga, LocalDateTime entrada, LocalDateTime saida, double valorPago,
 			List<ServicosAdicionais> servicosAdicionais){
-                return new UsoDeVagaCarro(vaga, entrada, saida, valorPago, servicosAdicionais);
+                return new UsoDeVagaCarro(vaga, entrada, saida, valorPago,servicosAdicionais);
             }
 }
