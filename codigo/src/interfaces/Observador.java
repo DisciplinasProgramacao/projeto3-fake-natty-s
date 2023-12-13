339905844b4ddee2a96aa001513eae03d246e74b
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-12-13 02:48:30 -0300

    Update Observador.java

--- codigo/src/interfaces/Observador.java
+++ codigo/src/interfaces/Observador.java
@@ -1,7 +1,5 @@
 package src.interfaces;
 
-import src.Exceptions.ExcecaoMesInvalido;
-
 public interface Observador {
-    public void atualizar(int mes) throws ExcecaoMesInvalido;
+    public void atualizar(int mes);
 }
