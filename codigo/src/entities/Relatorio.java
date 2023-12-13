26f522925e136451bf3a667c56c61407d5505204
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-12-13 02:46:28 -0300

    Update Relatorio.java

--- codigo/src/entities/Relatorio.java
+++ codigo/src/entities/Relatorio.java
@@ -2,33 +2,27 @@ package src.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import src.Exceptions.ExcecaoMesInvalido;
 import src.interfaces.*;
-import src.*;
 
 public class Relatorio implements Serializable, Observador {
     List<Observavel> observaveis;
-    Map<Integer, Set<Cliente>> top5clientes;
+    Map<Integer, List<Cliente>> top5clientes;
     Estacionamento estacionamento;
     
-    public Relatorio(Estacionamento estacionamento, Observavel observavel) {
+    public Relatorio(Estacionamento estacionamento) {
         this.estacionamento = estacionamento;
         this.observaveis = new ArrayList<>();
-        this.observaveis.add(observavel);
-        observavel.addObserver(this);
         top5clientes = new HashMap<>();
     }
 
     @Override
-    public void atualizar(int mes) throws ExcecaoMesInvalido {
-        Set<Cliente> topClientes = estacionamento.top5Clientes(mes);
+    public void atualizar(int mes)  {
+        List<Cliente> topClientes = estacionamento.top5Clientes(mes);
         top5clientes.put(mes, topClientes);
     }
 
@@ -40,11 +34,11 @@ public class Relatorio implements Serializable, Observador {
         this.observaveis = observaveis;
     }
 
-    public Map<Integer, Set<Cliente>> getTop5clientes() {
+    public Map<Integer, List<Cliente>> getTop5clientes() {
         return top5clientes;
     }
 
-    public void setTop5clientes(Map<Integer, Set<Cliente>> top5clientes) {
+    public void setTop5clientes(Map<Integer, List<Cliente>> top5clientes) {
         this.top5clientes = top5clientes;
     }
 
@@ -52,4 +46,9 @@ public class Relatorio implements Serializable, Observador {
         return estacionamento;
     }
     
+    public void addObservavel(Observavel observavel) {
+        observaveis.add(observavel);
+        observavel.addObserver(this);
+    }
+
 }
