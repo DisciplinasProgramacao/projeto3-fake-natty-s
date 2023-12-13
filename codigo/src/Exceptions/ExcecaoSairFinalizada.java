5d5748b6e2a8a4317af9992a74d3f43ca47d0e1e
GabrielPongelupe <130582324+GabrielPongelupe@users.noreply.github.com>
2023-12-13 02:48:07 -0300

    Update ExcecaoSairFinalizada.java

--- codigo/src/Exceptions/ExcecaoSairFinalizada.java
+++ codigo/src/Exceptions/ExcecaoSairFinalizada.java
@@ -3,6 +3,10 @@ import src.entities.Vaga;
 
 public class ExcecaoSairFinalizada extends Exception{
     public ExcecaoSairFinalizada (Vaga vaga){
-        super("Não foi possível sair da vaga " + vaga.getPosicao() + "pois este veículo não está ocupando ela.");
+        super("Não foi possível sair da vaga " + vaga.getPosicao() + " pois este veículo não está ocupando ela.");
+    }
+
+    public ExcecaoSairFinalizada(){
+        super("Não foi possivel sair da vaga. Veiculo não estacionado anteriormente");
     }
 }
